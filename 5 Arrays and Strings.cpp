3f6db4a42ebce2e4d3f456d93ff44ded9637d9e6
suyash gupta <suyashgupta0001@gmail.com>
2021-01-15 12:02:31 +0530

    fixed one away

--- 5 Arrays and Strings.cpp
+++ 5 Arrays and Strings.cpp
@@ -17,8 +17,9 @@ bool one_away(string s1, string s2)
 	
 	// if length of strings differ by over one => more than 1 edit required 
 	int diff = n1 - n2;
-	if(diff != 1 or diff != -1)
-		return false;
+	if(diff > 1 or diff < -1) return false;
+		
+	if(s1.compare(s2) == 0) return true; // if both strings are identical, return true
 	
 	bool one_edit_made = false;
 	
@@ -40,42 +41,64 @@ bool one_away(string s1, string s2)
 	     // these 2 edits are basically the same as (removing from the longer string) == (inserting into the shorter string)
 	     // here, I'll insert into the shorter string
 	{
-		int longer_length;
-		string shorter_string;
-		if(n1>n2)
+		int n, smaller_s; // smaller index, smaller string
+		if(n1<n2)
 			{
-				longer_length = n1; 
-				shorter_string = "s2";
+				n = n1;
+				smaller_s = 1;
 			}
 		else
 			{
-				longer_length = n2; 
-				shorter_string = "s1";
+				n = n2;
+				smaller_s = 2;
 			}
-		
-		for(int i = 0; i<longer_length; i++)
+			
+		for(int i = 0; i<n; i++) // we traverse up to the smaller index
 		{
 			if(s1[i] != s2[i] and (not one_edit_made))
 			{
 				one_edit_made = true;
-				// now insert into the shorter string
-				if(shorter_string == "s1")
-					s1.insert(i, string(1, s2[i]));
-				else if (shorter_string == "s2")
-					s1.insert(i, string(1, s1[i]));
+				// insert into the smaller string -> now the s1 and s2 will become of same size
+				if(smaller_s == 1)
+					s1.insert(i, "x"); // correct thing in place of x = string(1, s2[i])
+				else if (smaller_s == 2)
+					s2.insert(i, "x"); // correct thing in place of x = string(1, s1[i])	
 			}
-			else if(s1[i] != s2[i] and one_edit_made)
+			else if (s1[i] != s2[i] and one_edit_made) return false; 
+		}
+		
+//		 after traversing through the loop, there are 2 cases
+//		 (1) an element was inserted in the smaller string 
+//				-> now the last elements of both strings needs to be same to return true
+//				-> else return false
+//		 (2) no element was inserted in the smaller string 
+//				-> now an element (= last elemt of larger string) can be inserted in the smaller string 
+//				-> return true
+		
+		// update n1,n2 with new lengths (if any)
+		n1 = s1.length();
+		n2 = s1.length();
+		
+		if(n1 == n2) // (1)
+		{
+			if(s1[n1] == s1[n2])
+				return true;
+			else
 				return false;
 		}
-		return true;
+		else // (2) 
+			return true;
 	}	
 }
 
 int main()
 {
-	cout<<bool_to_string(one_away("heeello", "hello")); // ok
-	cout<<bool_to_string(one_away("hEllo", "hello"));
-//	cout<<bool_to_string(one_away("john", "johnny "));
-//	cout<<bool_to_string(one_away("onestring", "nestring"));
+	cout<<bool_to_string(one_away("heexello", "hello")); // false
+	cout<<bool_to_string(one_away("hellobro", "helloman")); // false
+	cout<<bool_to_string(one_away("bILLY", "bILLY")); // true
+	cout<<bool_to_string(one_away("hEllo", "hello")); // true
+	cout<<bool_to_string(one_away("hllo", "hello")); // true
+	cout<<bool_to_string(one_away("john", "johny")); // true
+	cout<<bool_to_string(one_away("onestring", "nestring")); // true
 	return 0; 
 }
