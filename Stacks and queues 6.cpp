31db3a80ca7ae2f89e4e6f08f811370f8981e962
Suyash Gupta <suyashgupta0001@gmail.com>
2021-02-12 11:07:42 +0530

    added CTCI approach

--- Stacks and queues 6.cpp
+++ Stacks and queues 6.cpp
@@ -4,9 +4,73 @@
 
 using namespace std;
 
+// CTCI's way
 class Queue_
 {
-	// fill this
+	// 2 seperate stacks for cats and dogs
+	private:
+		vector<pair<int,string>> dogs;
+		vector<pair<int,string>> cats;
+		int age = 0; // will be use to find the oldest animal to dequeue
+
+	public:
+		void enqueue(string animal_name, string animal_type)
+		{
+			if(animal_type == "cat")
+				cats.push_back({age, animal_name});
+			else if(animal_type == "dog")
+				dogs.push_back({age, animal_name});
+			age--;
+			// else if it's neither a cat not a dog, do nothing
+		}
+		string dequeue_any()
+		{
+			// take a look at the tops of both stacks,
+			// dequeue the older one
+			string temp;
+			if(cats.empty() and dogs.empty())
+				return "no animal left";
+			else if(cats.empty() and !dogs.empty()) // only cats left
+			{
+				temp = cats.begin()->second;
+				cats.erase(cats.begin());
+			}
+			else if(!cats.empty() and dogs.empty()) // only dogs left
+			{
+				temp = dogs.begin()->second;
+				dogs.erase(dogs.begin());
+			}
+			else // both cats and dogs are there
+			{
+				if(cats.begin()->first > dogs.begin()->first)
+				{
+					temp = cats.begin()->second;
+					cats.erase(cats.begin());
+				}
+				else
+				{
+					temp = dogs.begin()->second;
+					dogs.erase(dogs.begin());
+				}
+			}
+			return temp;
+		}
+		string dequeue_cat()
+		{
+			if(cats.empty())
+				return "no cat left";
+			string temp = cats.begin()->second;
+			cats.erase(cats.begin());
+			return temp;
+		}
+		string dequeue_dog()
+		{
+			if(dogs.empty())
+				return "no dog left";
+			string temp = dogs.begin()->second;
+			dogs.erase(dogs.begin());
+			return temp;
+		}
 };
 
 // My approach: Not as good as CTCI's as it requires iteration through the vector in dequeue_cat(), dequeue_dog().
@@ -14,12 +78,13 @@ class Queue
 {
 	private:
 		vector<pair<string, string>> v; // animal_name, animal_type
+		// no need to declare f, r as we have v.begin() and v.end() 
 	public:
 		bool empty()
 		{
 			return(v.size() == 0);
 		}
-		void enqueue_any(string animal_name, string animal_type)
+		void enqueue(string animal_name, string animal_type)
 		{
 			// not checking for full as using vector
 			v.push_back({animal_name, animal_type});
@@ -84,13 +149,13 @@ class Queue
 
 int main()
 {
-	Queue q;
-	cout<<q.empty()<<"\n"; // prints 1
-	q.enqueue_any("tommy", "dog");
-	q.enqueue_any("tom", "cat");
-	q.enqueue_any("bob", "cat");
-	q.enqueue_any("tiffany", "dog"); 
-	cout<<q.empty()<<"\n"; // prints 0
+	Queue_ q;
+//	cout<<q.empty()<<"\n"; // prints 1
+	q.enqueue("tommy", "dog");
+	q.enqueue("tom", "cat");
+	q.enqueue("bob", "cat");
+	q.enqueue("tiffany", "dog"); 
+//	cout<<q.empty()<<"\n"; // prints 0
 	cout<<q.dequeue_any()<<"\n"; // prints tommy
 	cout<<q.dequeue_dog()<<"\n"; // prints tiffany
 	cout<<q.dequeue_dog()<<"\n"; // prints no dog
